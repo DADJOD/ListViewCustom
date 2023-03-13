b62a2a9ba01c2327326bb2390fbb026e70cf6818
DADJOD <kamu57@mail.ru>
2023-03-13 19:53:44 +0300

    commit_3
    commit_3

--- app/src/main/java/com/example/listviewcustom/DetailView.kt
+++ app/src/main/java/com/example/listviewcustom/DetailView.kt
@@ -3,6 +3,18 @@ package com.example.listviewcustom
 import android.content.Context
 import android.util.AttributeSet
 import android.widget.LinearLayout
+import android.widget.TextView
 
 class DetailView(context: Context?, attrs: AttributeSet?) : LinearLayout(context, attrs) {
+    private lateinit var itemView: TextView
+
+    override fun onFinishInflate() {
+        super.onFinishInflate()
+
+        itemView = findViewById(R.id.info)
+    }
+
+    fun showItem(item: String) {
+        itemView.text = item
+    }
 }
